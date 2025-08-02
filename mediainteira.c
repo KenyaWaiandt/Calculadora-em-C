78e7e90983a24e5d4dafb698202f471dea296261
Kenya Lauanda Reinke Waiandt <waiandtkenya@gmail.com>
2025-08-02 13:55:35 -0300

    Delete mediainteira.c

--- mediainteira.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int x, y;
-    printf("Digite o primeiro valor:\n");
-   scanf("%d", &x);
-   printf("Digite o segundo valor:\n");
-   scanf("%d", &y);
-    printf( " Soma = %d\n", x+y);
-}
